aa5525e3b8b6b82ab8ee023ad6127714a653d748
KBG360 <55129891+KBG360@users.noreply.github.com>
2020-12-03 22:06:36 +0100

    day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,31 +1,30 @@
-amount_of_trees = 0
-right = 1
-down = 2
+right = 3
+down = 1
+
+goto_spot_y = down
 current_spot_x = 0
-current_spot_y = 0
-where_you_have_to_be = down
-rowNo = 0
+current_row = 0
+amount_of_trees = 0
 
 # Open input and assign variables
-with open('input2.txt') as f:
+with open('input.txt') as f:
     lines = f.readlines()
     num_lines = sum(1 for line in open('input.txt'))
 
 for x in lines:
-    rowNo += 1
-    row = list(x)
-    if current_spot_y == where_you_have_to_be:
+    current_row += 1
+    row = list(x.strip())
+    width = len(row)
+    if current_spot_x >= width:
+        current_spot_x = current_spot_x % width
+    if current_row == goto_spot_y:
         if row[current_spot_x] == "#":
-            current_spot_x += right
-            current_spot_y += down
-            where_you_have_to_be += down
             amount_of_trees += 1
-        else:
-            current_spot_y += down
             current_spot_x += right
-            where_you_have_to_be += down
+        else:
+            current_spot_x += right
     else:
-        current_spot_y += down
         current_spot_x += right
-
+        current_row += down
+        
 print(amount_of_trees)
